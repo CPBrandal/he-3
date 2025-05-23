d6c6e31577597c16a1174f1b98a8d06fb6b89cac
CPBrandal <philip.brandal@gmail.com>
2025-05-23 14:51:26 +0200

    Fixed for tractor

--- in5050-codec63-dolphin-sisci/run.sh
+++ in5050-codec63-dolphin-sisci/run.sh
@@ -19,7 +19,8 @@ if [ ! -d $SRC_DIR/$SUBDIR ]; then
 fi
 
 PC_CMD="c63client"
-PC_ARGS="/opt/Media/foreman.yuv -o output -w 352 -h 288"
+#PC_ARGS="/opt/Media/foreman.yuv -f 120 -o output -w 352 -h 288"
+PC_ARGS="/opt/Media/tractor.yuv -f 250 -o tractor -w 1920 -h 1080"
 
 DATE=$(date -u +%Y%m%d-%H%M%S)
 RSYNC_ARGS="-rt --exclude=logs/ --exclude=.*"
